b92eb40383cc96c226637d7190a659634e1b2841
reizhafajrian <reizha77@gmail.com>
2020-11-26 17:25:54 +0700

    push

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				}
 			}
 		};
-		xhttp.open("GET", 'pages/'+page+'.html', true);
+		xhttp.open("GET", './pages/'+page+'.html', true);
 		xhttp.send();
 	}
 
